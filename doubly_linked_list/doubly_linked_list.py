4b165fa5a2df4b9918232969383512ce1d0a7b49
Sherexmykes <sherexmykes@gmail.com>
2020-06-19 19:42:05 -0400

    finished DLL

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -109,13 +109,35 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+       if node is not self.tail:
+            self.delete(node)
+            self.add_to_tail(node.value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        self.length -= 1
+
+        if self.tail is self.head:
+            self.tail = self.head = None
+        elif self.head is node:
+            self.head = self.head.next
+            node.delete()
+        elif self.tail is node:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        if self.head:
+            node = self.head
+            current_max = node.value
+
+            while node:
+                if node.value > current_max:
+                    current_max = node.value
+                node = node.next
+
+            return current_max
